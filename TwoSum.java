02699367304082ded1055031761490706a6f7e7b
IdanG97 <138139373+IdanG97@users.noreply.github.com>
2025-01-21 18:49:20 +0200

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -3,25 +3,17 @@ import java.util.Map;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        // Create a HashMap to store numbers and their indices
         Map<Integer, Integer> map = new HashMap<>();
-
-        // Iterate through the array
+        
         for (int i = 0; i < nums.length; i++) {
-            // Calculate the complement needed to reach the target
             int complement = target - nums[i];
 
-            // Check if the complement is already in the map
             if (map.containsKey(complement)) {
-                // If found, return the indices
                 return new int[] { map.get(complement), i };
             }
-
-            // Add the current number and its index to the map
             map.put(nums[i], i);
         }
 
-        // Return an empty array if no solution (not expected in valid input)
         return new int[0];
     }
 }
